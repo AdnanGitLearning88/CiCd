1d0801f73fc1d0f98c0dd3d92d59a8c8f5fb8662
adshaikh <adshaikh@INAILABNMS26.ecitele.com>
2022-01-05 14:33:44 +0530

    New File to commit

---
+++ src/test/java/package1/Test2.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void testName() throws Exception {
+		System.out.println("Testing....");
+	}
+	
+}
